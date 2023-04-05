59b52b93761ce7abcea4c6d0cfc7e1f3861cdf59
rezaramadhanirianto <rezaramadhanirianto@gmail.com>
2023-04-06 01:45:48 +0700

    add gateway and service discovery

--- inventory-service/src/main/java/com/rezaramadhanirianto/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/rezaramadhanirianto/inventoryservice/controller/InventoryController.java
@@ -1,9 +1,12 @@
 package com.rezaramadhanirianto.inventoryservice.controller;
 
+import com.rezaramadhanirianto.inventoryservice.dto.InventoryResponse;
 import com.rezaramadhanirianto.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -12,9 +15,10 @@ public class InventoryController {
 
     private final InventoryService inventoryService;
 
-    @GetMapping("/{sku-code}")
+    // /api/inventory?sku-code=ipad_10&sku-code=ipad_11
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode){
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode){
         return inventoryService.isInStock(skuCode);
     }
 }
--- order-service/src/main/java/com/rezaramadhanirianto/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/rezaramadhanirianto/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.rezaramadhanirianto.orderservice.service;
 
+import com.rezaramadhanirianto.orderservice.dto.InventoryResponse;
 import com.rezaramadhanirianto.orderservice.dto.OrderLineItemDto;
 import com.rezaramadhanirianto.orderservice.dto.OrderRequest;
 import com.rezaramadhanirianto.orderservice.model.Order;
@@ -8,7 +9,9 @@ import com.rezaramadhanirianto.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -18,6 +21,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest){
         var order = new Order();
@@ -27,7 +31,29 @@ public class OrderService {
                 .map(this::mapToDto)
                 .toList();
         order.setOrderLineItemList(orderLineItems);
-        orderRepository.save(order);
+
+        List<String> skuCodes = order.getOrderLineItemList()
+                .stream()
+                .map(OrderLineItem::getSkuCode)
+                .toList();
+
+        // call inventory service
+        // if program is in stock
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block(); // make synchronous
+
+        boolean allProductIsInStock = Arrays.stream(inventoryResponseArray)
+                .allMatch(InventoryResponse::getIsInStock);
+
+        if(allProductIsInStock){
+            orderRepository.save(order);
+        }else{
+            throw new IllegalArgumentException("Product is empty, please try again later");
+        }
     }
 
     private OrderLineItem mapToDto(OrderLineItemDto orderLineItemDTO) {
